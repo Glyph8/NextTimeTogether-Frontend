15b9938a29ef9b5b26219025df3cae0e70e8e78c
glyph8 <newdy0808@gmail.com>
2025-08-26 00:20:11 +0900

    feat: 그룹 생성 페이지 ui 구현

---
+++ src/app/(dashboard)/groups/create/page.tsx
@@ -0,0 +1,81 @@
+"use client"
+
+import Image from "next/image";
+import Header from "@/components/ui/header/Header";
+import X from "@/assets/svgs/icons/x-black.svg"
+import XWhite from "@/assets/svgs/icons/x-white.svg"
+import Plus from "@/assets/svgs/icons/plus-white.svg"
+import { Button } from "@/components/ui/button/Button";
+import { useState } from "react";
+export default function CreateGroupPage() {
+
+    const [groupName, setGroupName] = useState("");
+    const [groupDescription, setGroupDescription] = useState("");
+
+    const handleCreateGroup = () => {
+
+    }
+    return (
+        <div className="flex flex-col w-full flex-1 bg-white pb-5">
+            <Header
+                leftChild={
+                    <X />
+                }
+                title={
+                    <h1 className="text-center text-lg font-medium leading-tight">
+                        그룹 만들기
+                    </h1>
+                } />
+
+            <div className="w-full flex justify-center items-center px-40 pt-5 pb-3.5 relative">
+                <div className="w-16 h-16 rounded-[8px] border-[#E4E4E4] bg-gray-3 relative">
+                    <Image src={"https://placehold.co/64x64"} alt="프로필" width={64} height={64} />
+                    <button className="w-6 h-6 rounded-full bg-gray-2 flex justify-center items-center
+                absolute top-11 left-11">
+                        <Plus />
+                    </button>
+                </div>
+            </div>
+
+            <div className="flex-1 w-full h-full p-4 flex flex-col justify-between items-start">
+
+                <div className="w-full flex flex-col gap-8">
+                    <div className="w-full flex flex-col gap-3">
+                        <span className="text-gray-1 text-sm font-normal leading-tight">
+                            그룹명
+                        </span>
+                        <div className="w-full flex justify-between relative">
+                            <input placeholder="그룹명을 입력해주세요."
+                                className="w-full py-2.5 text-black-1 text-base font-medium leading-tight border-b-1 focus:border-b-main"
+                                onChange={(e) => setGroupName(e.target.value)} value={groupName}/>
+
+                            {groupName !== "" && 
+                            <button className="absolute right-1 top-3 w-5 h-5 bg-gray-3 rounded-full flex justify-center items-center"
+                            onClick={()=>setGroupName("")}>
+                                <XWhite />
+                            </button>}
+                        </div>
+                    </div>
+
+                    <div className="w-full flex flex-col gap-3">
+                        <span className="text-gray-1 text-sm font-normal leading-tight">
+                            어떤 그룹인가요?
+                        </span>
+                        <div className="w-full flex justify-between relative">
+                            <input placeholder="그룹 설명을 입력해주세요."
+                                className="w-full py-2.5 text-black-1 text-base font-medium leading-tight border-b-1 focus:border-b-main"
+                                onChange={(e) => setGroupDescription(e.target.value)} value={groupDescription} />
+                            {groupDescription !== "" && 
+                            <button className="absolute right-1 top-3 w-5 h-5 bg-gray-3 rounded-full flex justify-center items-center"
+                            onClick={()=>setGroupDescription("")}>
+                                <XWhite />
+                            </button>}
+                        </div>
+                    </div>
+                </div>
+
+                <Button text={"그룹 만들기"} disabled={false} onClick={handleCreateGroup} />
+            </div>
+        </div>
+    )
+}
--- src/app/(dashboard)/groups/page.tsx
+++ src/app/(dashboard)/groups/page.tsx
@@ -4,9 +4,14 @@ import Header from "@/components/ui/header/Header";
 import { GroupItem } from "./GroupItem";
 import { ExitGroupDialog } from "./ExitGroupDialog";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function GroupsPage() {
-     const [isOpenDialog, setIsOpenDialog] = useState(false);
+    const router = useRouter();
+    const [isOpenDialog, setIsOpenDialog] = useState(false);
+    const handleCreateBtn = () => {
+        router.push("/groups/create");
+    }
     return (
         <div className="flex flex-col w-full flex-1 bg-[#F9F9F9]">
             <Header title={
@@ -16,20 +21,21 @@ export default function GroupsPage() {
             } />
 
             <div className="w-full h-19 flex justify-end items-center px-4">
-                <button className="inline-flex w-30 px-5 py-2.5 bg-main rounded-[8px] text-center justify-center text-white text-base font-medium leading-tight">
+                <button className="inline-flex w-30 px-5 py-2.5 bg-main rounded-[8px] text-center justify-center text-white text-base font-medium leading-tight"
+                    onClick={handleCreateBtn}>
                     그룹 만들기
                 </button>
             </div>
 
             <div className="w-full flex flex-col gap-2 px-4">
-                <GroupItem image={"https://placehold.co/64x64"} title={"팀1"} description={"2024-2학기 소프트웨어공학 팀플"} 
-                members={"김OO, 김OO, 박OO, 이OO"} setIsOpen={setIsOpenDialog} />
-                <GroupItem image={"https://placehold.co/64x64"} title={"팀1"} description={"2024-2학기 소프트웨어공학 팀플"} 
-                members={"김OO, 김OO, 박OO, 이OO"} setIsOpen={setIsOpenDialog} />
-                <GroupItem image={"https://placehold.co/64x64"} title={"팀1"} description={"2024-2학기 소프트웨어공학 팀플"} 
-                members={"김OO, 김OO, 박OO, 이OO"} setIsOpen={setIsOpenDialog} />
-                <GroupItem image={"https://placehold.co/64x64"} title={"팀1"} description={"2024-2학기 소프트웨어공학 팀플"} 
-                members={"김OO, 김OO, 박OO, 이OO"} setIsOpen={setIsOpenDialog} />
+                <GroupItem image={"https://placehold.co/64x64"} title={"팀1"} description={"2024-2학기 소프트웨어공학 팀플"}
+                    members={"김OO, 김OO, 박OO, 이OO"} setIsOpen={setIsOpenDialog} />
+                <GroupItem image={"https://placehold.co/64x64"} title={"팀1"} description={"2024-2학기 소프트웨어공학 팀플"}
+                    members={"김OO, 김OO, 박OO, 이OO"} setIsOpen={setIsOpenDialog} />
+                <GroupItem image={"https://placehold.co/64x64"} title={"팀1"} description={"2024-2학기 소프트웨어공학 팀플"}
+                    members={"김OO, 김OO, 박OO, 이OO"} setIsOpen={setIsOpenDialog} />
+                <GroupItem image={"https://placehold.co/64x64"} title={"팀1"} description={"2024-2학기 소프트웨어공학 팀플"}
+                    members={"김OO, 김OO, 박OO, 이OO"} setIsOpen={setIsOpenDialog} />
             </div>
             <ExitGroupDialog isOpen={isOpenDialog} setIsOpen={setIsOpenDialog} />
         </div>
