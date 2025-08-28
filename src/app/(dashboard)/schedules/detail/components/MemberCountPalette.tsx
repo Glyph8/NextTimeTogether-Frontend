0114daa6b9bfb04a8bb4d22c8ef0e8d88dff2ae1
glyph8 <newdy0808@gmail.com>
2025-08-29 01:54:05 +0900

    feat: 약속 언제, 어디서 게시판 뼈대 작성

---
+++ src/app/(dashboard)/schedules/detail/components/MemberCountPalette.tsx
@@ -0,0 +1,7 @@
+export const MemberCountPalette = () =>{
+    return(
+        <div>
+            0/0 ~~~~~~~~ 5/5
+        </div>
+    )
+}
---
+++ src/app/(dashboard)/schedules/detail/components/Where2Meet.tsx
@@ -0,0 +1,14 @@
+export const Where2Meet = () =>{
+    return(
+        <div className="bg-[#F9F9F9]">
+            <div className="flex flex-col gap-3 px-4 py-5 items-center bg-white">
+                <div className="text-center text-black-1 text-lg font-semibold leading-tight">
+                    장소 게시판
+                </div>
+                <div className="text-center text-gray-2 text-sm font-normal leading-tight">
+                    모임을 원하는 장소에 투표해주세요.
+                </div>
+            </div>
+        </div>
+    )
+}
