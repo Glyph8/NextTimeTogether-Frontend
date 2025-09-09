c0e2037e1f2b2e4c69f5fa60dd708dd4eb37c122
Glyph8 <newdy0808@gmail.com>
2025-09-09 13:11:46 +0900

    feat: toast 메세지 버튼 보다 위로 이동

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -12,6 +12,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group bg-black"
+      offset={80}
       toastOptions={{
         classNames: {
           toast:
